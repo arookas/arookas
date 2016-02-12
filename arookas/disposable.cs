9621c9bdc9d3575e764130a63c282287390396b4
arookas <arookas@outlook.com>
2016-02-11 23:23:26 -0500

    Added disposable base class

---
+++ arookas/disposable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace arookas {
+	public abstract class aDisposable : IDisposable {
+		bool mDisposed;
+
+		public bool IsDisposed {
+			get { return mDisposed; }
+		}
+
+		protected aDisposable() { }
+		~aDisposable() {
+			if (!IsDisposed) {
+				mDisposed |= Dispose(true);
+			}
+		}
+
+		public void Dispose() {
+			if (!mDisposed) {
+				mDisposed |= Dispose(false);
+			}
+		}
+		protected abstract bool Dispose(bool destructor);
+	}
+}
