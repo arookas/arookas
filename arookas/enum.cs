e1c7df1c8366ed7f9eb5a688ece550643527fea0
arookas <arookas@outlook.com>
2016-02-11 23:24:37 -0500

    Added enum utility

---
+++ arookas/enum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace arookas {
+	public static class aEnum {
+		public static bool IsDefined<T>(this T enumValue)
+			where T : struct, IComparable, IFormattable, IConvertible {
+			return Enum.IsDefined(typeof(T), enumValue);
+		}
+	}
+}
