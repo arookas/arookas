8b4e8a5014d28c8febf40126dd06416f8ba24ce8
arookas <arookas@outlook.com>
2016-02-11 23:31:10 -0500

    Added reflection utility

---
+++ arookas/reflection/marshal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace arookas.Reflection {
+	public static class aMarshal {
+		public static IntPtr Allocate<T>() {
+			return Marshal.AllocHGlobal(Marshal.SizeOf(typeof(T)));
+		}
+		public static int SizeOf<T>() {
+			return Marshal.SizeOf(typeof(T));
+		}
+		public static T ToStructure<T>(this IntPtr pointer) {
+			return (T)Marshal.PtrToStructure(pointer, typeof(T));
+		}
+	}
+}
---
+++ arookas/reflection/reflection.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace arookas.Reflection {
+	public static class aReflection {
+		public static T CreateInstance<T>(params object[] args) {
+			try {
+				return (T)Activator.CreateInstance(typeof(T), args);
+			}
+			catch (InvalidCastException) {
+				return default(T);
+			}
+		}
+		public static T CreateInstance<T>(Type type, params object[] args) {
+#if DEBUG
+			aError.CheckNull(type, "type");
+#endif
+			try {
+				return (T)Activator.CreateInstance(type, args);
+			}
+			catch (InvalidCastException) {
+				return default(T);
+			}
+		}
+
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute>(this Assembly assembly)
+			where TAttribute : Attribute {
+			return GetTypesWithAttribute<TAttribute, Object>(assembly, true);
+		}
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute>(this Assembly assembly, bool publicOnly)
+			where TAttribute : Attribute {
+			return GetTypesWithAttribute<TAttribute, Object>(assembly, publicOnly, type => !type.IsAbstract);
+		}
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute>(this Assembly assembly, Predicate<Type> predicate)
+			where TAttribute : Attribute {
+			return GetTypesWithAttribute<TAttribute, Object>(assembly, true, predicate);
+		}
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute>(this Assembly assembly, bool publicOnly, Predicate<Type> predicate)
+			where TAttribute : Attribute {
+			return GetTypesWithAttribute<TAttribute, Object>(assembly, publicOnly, predicate);
+		}
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute, TBase>(this Assembly assembly)
+			where TAttribute : Attribute {
+			return GetTypesWithAttribute<TAttribute, TBase>(assembly, true);
+		}
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute, TBase>(this Assembly assembly, bool publicOnly)
+			where TAttribute : Attribute {
+			return GetTypesWithAttribute<TAttribute, TBase>(assembly, publicOnly, type => !type.IsAbstract);
+		}
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute, TBase>(this Assembly assembly, Predicate<Type> predicate)
+			where TAttribute : Attribute {
+			return GetTypesWithAttribute<TAttribute, TBase>(assembly, true, predicate);
+		}
+		public static Dictionary<TAttribute, Type> GetTypesWithAttribute<TAttribute, TBase>(this Assembly assembly, bool publicOnly, Predicate<Type> predicate)
+			where TAttribute : Attribute {
+			if (assembly == null) {
+				throw new ArgumentNullException("assembly");
+			}
+			if (predicate == null) {
+				throw new ArgumentNullException("predicate");
+			}
+			var foundTypes =
+				from type in (publicOnly ? assembly.GetExportedTypes() : assembly.GetTypes())
+				where typeof(TBase).IsAssignableFrom(type) // IsSubclassOf
+				where predicate(type)
+				let attributes = type.GetCustomAttributes(typeof(TAttribute), false)
+				where attributes.Length > 0
+				let attribute = (attributes[0] as TAttribute)
+				select new KeyValuePair<TAttribute, Type>(attribute, type);
+			return foundTypes.ToDictionary(k => k.Key, e => e.Value);
+		}
+
+		public static bool ImplementsInterface<TInterface>(this Type type)
+			where TInterface : class {
+			if (type == null) {
+				throw new ArgumentNullException("type");
+			}
+			var interfaceType = typeof(TInterface);
+			if (!interfaceType.IsInterface) {
+				throw new ArgumentException(String.Format("The specified type '{0}' is not an interface.", interfaceType.Name), "type");
+			}
+			return (type.GetInterface(interfaceType.Name) != null);
+		}
+	}
+}
