8fe5c7bdb5d1f699d9f87c03ed9fe141d8148f1e
arookas <arookas@outlook.com>
2016-12-18 16:09:32 -0500

    Fixed: padding length check was too late

--- arookas/io/binary/writer.padding.cs
+++ arookas/io/binary/writer.padding.cs
@@ -4,8 +4,9 @@ namespace arookas.IO.Binary {
 	public static partial class aBinaryWriterUtil {
 		public static void WritePadding(this aBinaryWriter writer, int multiple, byte padding) {
 			aError.CheckNull(writer, "writer");
-			var length = (int)(multiple - (writer.Position % multiple));
-			if (length > 0) {
+			var remainder = (writer.Position % multiple);
+			if (remainder > 0) {
+				var length = (multiple - remainder);
 				var buffer = new byte[length];
 				if (padding != 0) {
 					for (var i = 0; i < length; ++i) {
@@ -19,9 +20,10 @@ namespace arookas.IO.Binary {
 			aError.CheckNull(writer, "writer");
 			aError.CheckNull(padding, "padding");
 			aError.Check<ArgumentException>(padding.Length > 0, "Array is empty.", "padding");
-			var length = (int)(multiple - (writer.Position % multiple));
-			var buffer = new byte[length];
-			if (length > 0) {
+			var remainder = (writer.Position % multiple);
+			if (remainder > 0) {
+				var length = (multiple - remainder);
+				var buffer = new byte[length];
 				var p = 0;
 				for (var i = 0; i < length; ++i) {
 					buffer[i] = padding[p];
@@ -30,8 +32,8 @@ namespace arookas.IO.Binary {
 						p = 0;
 					}
 				}
+				writer.Write8s(buffer);
 			}
-			writer.Write8s(buffer);
 		}
 	}
 }
