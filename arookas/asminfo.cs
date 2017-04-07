0ebe9c798245b6e0a81b5f4aa66acc0eef303ece
arookas <arookas@outlook.com>
2017-04-07 18:00:24 -0400

    Add default assembly information file

---
+++ arookas/asminfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("arookas library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("arookas")]
+[assembly: AssemblyProduct("arookas library")]
+[assembly: AssemblyCopyright("Copyright © 2014-2017 arookas")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("51531489-0ca0-4ad8-bcc9-5d17927ac04b")]
